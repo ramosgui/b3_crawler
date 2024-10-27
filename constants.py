50e543797f2cc7a8447e1a1d29adbaaec8144d0f
Guilherme <guilherme.ramos@outlook.com>
2024-10-27 20:34:46 -0300

    - Implementado tentativa de `async` no processamento da transação.
    - Implementado tempo de execução do processamento da transação;
    - Adicionado `driver.quit()` ao finalizar navegação com sucesso;
    - Adicionado parâmetro `raise_error` na função `get_element_by_class`;
    - Adicionado mais logs de execução da aplicação;
    - Adicionado `return` na função `click_element`;
    - Alterado função `abrir_tela_movimentacao` para utilizar a função `click_element` em vários pontos;
    - Criado arquivo `constants.py`.

---
+++ constants.py
@@ -0,0 +1,14 @@
+FIRST_PAGE_URL = "https://www.investidor.b3.com.br/login?utm_source=B3_MVP&utm_medium=HM_PF&utm_campaign=menu"
+MOVIMENTACAO_URL = "https://www.investidor.b3.com.br/extrato/movimentacao"
+
+FILTER_BUTTON_XPATH = "/html/body/app-root/app-core/div/div/app-extrato/div/app-movimentacao/div/app-tabela-filtro/div/div/b3-button[1]/button"
+MODAL_FILTER_BUTTON_XPATH = "/html/body/app-root/app-core/div/div/app-extrato/div/app-movimentacao/app-movimentacao-filtrar/div/b3-modal-drawer/div[1]/div[2]/div/div[2]/b3-button[1]/button"
+START_LIMIT_STRING_XPATH = "/html/body/app-root/app-core/div/div/app-extrato/div/app-movimentacao/app-movimentacao-filtrar/div/b3-modal-drawer/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/p"
+END_LIMIT_STRING_XPATH = '/html/body/app-root/app-core/div/div/app-extrato/div/app-movimentacao/app-movimentacao-filtrar/div/b3-modal-drawer/div[1]/div[2]/div/div[1]/div/div[2]/div[1]/app-datepicker-composto/div/form/b3-datepicker/div/b3-message/small'
+USER_INPUT_XPATH = "//*[@id='investidor']"
+ENTER_BUTTON_XPATH = "/html/body/app-root/app-landing-page/div/div[2]/aside/form/b3-button[1]/button/div"
+REJECT_COOKIES_XPATH = "//*[@id='onetrust-reject-all-handler']"
+SKIP_TOUR_XPATH = "/html/body/app-root/app-core/app-tour-guiado-inicio/div/div/div/b3-button[1]/button/div/span"
+CONTINUE_BUTTON_XPATH = "//*[@id='Btn_CONTINUE']"
+PASSWORD_INPUT_XPATH = "//*[@id='PASS_INPUT']"
+NEXT_PAGE_BUTTON_XPATH = "/html/body/app-root/app-core/div/div/app-extrato/div/app-movimentacao/div/div/*/b3-paginator/div/nav/ul/b3-paginator-lateral-button[3]/li/a/b3-icon/span"
